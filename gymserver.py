e1b54938fcf4c3208a26326ae1c2c3fcd524dc1c
Gerry Donnelly <donneger33@gmail.com>
2021-12-06 08:08:19 +0000

    Updating code for functions.

--- gymserver.py
+++ gymserver.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for, request, redirect, abort, jsonify
 from gymDAO import gymDao
+from datetime import date, datetime
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
@@ -11,55 +12,62 @@ def index():
     return "hello world"
 
 # Get all Data
-@app.route('/books')
+@app.route('/gymbooking')
 def getAll():
     return jsonify(gymDao.getAll())
 
 # Get a specific record
-@app.route('/books/<int:ISBN>')
+@app.route('/gymbooking/<int:id>')
 def findById(ISBN):
-    return jsonify(gymDao.findById(ISBN))
+    return jsonify(gymDao.findById(id))
 
 
 # Add a new record
-@app.route('/books', methods = ['POST'])
+@app.route('/gymbooking', methods = ['POST'])
 def create():
     if not request.json:
         abort(400)
-    book = {
-    "ISBN": request.json['ISBN'], 
-    "title": request.json["title"], 
-    "author": request.json["author"],
-    "price": request.json["price"]
+    booking = {
+    "membername": request.json['membername'], 
+    "age": request.json["age"], 
+    "class": request.json["class"],
+    "classdate": request.json["classdate"],
+    "classtime": request.json["classtime"],
+    "instructor": request.json["instructor"]
     }
-    return jsonify(gymDao.create(book))
+    return jsonify(gymDao.create(booking))
 
 # Update a record
-@app.route('/books/<int:ISBN>', methods = ['PUT'])
-def update(ISBN):
-    foundBook = gymDao.findById(ISBN)
-    print(foundBook)
-    if len(foundBook) == 0:
+@app.route('/gymbooking/<int:id>', methods = ['PUT'])
+def update(id):
+    foundBooking = gymDao.findById(id)
+    print(foundBooking)
+    if len(foundBooking) == 0:
         return jsonify({}), 404      
-    currentBook = foundBook
-    if 'title' in request.json:
-        currentBook['title'] = request.json['title']
-    if 'author' in request.json:
-        currentBook['author'] = request.json['author']
-    if 'price' in request.json:
-            currentBook['price'] = request.json['price']
-    gymDao.update(currentBook)
-
-    return jsonify(currentBook)
+    currentBooking = foundBooking
+    if 'membername' in request.json:
+        currentBooking['membername'] = request.json['membername']
+    if 'age' in request.json:
+        currentBooking['age'] = request.json['age']
+    if 'class' in request.json:
+            currentBooking['class'] = request.json['class']
+    if 'classdate' in request.json:
+            currentBooking['classdate'] = request.json['classdate']
+    if 'classtime' in request.json:
+        currentBooking['classtime'] = request.json['classtime']
+    if 'instructor' in request.json:
+            currentBooking['instructor'] = request.json['instructor']
+    gymDao.update(currentBooking)
+    return jsonify(currentBooking)
 
 # Delete a record
-@app.route('/books/<int:ISBN>', methods = ['DELETE'])
-def delete(ISBN):
-    foundBook = gymDao.findById(ISBN)
-    print(foundBook)
-    if len(foundBook) == 0:
+@app.route('/gymbooking/<int:id>', methods = ['DELETE'])
+def delete(id):
+    foundBooking = gymDao.findById(id)
+    print(foundBooking)
+    if len(foundBooking) == 0:
         return jsonify({}), 404
-    gymDao.delete(ISBN)
+    gymDao.delete(id)
     return jsonify({"done":True})
 
 
